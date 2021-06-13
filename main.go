d866026adbc81b1b61269e0c95cb3af77aa12aad
zen <zen@zen>
2021-06-12 19:36:59 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//health check:  https://.../mcf2/healthcheck
+
+//payments end point : https://.../mcf2/v1.0/payments
+
+const address string = ":8080"
+
+func payments(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println(w, r.URL.Path)
+	fmt.Fprintf(w, "%s s-print %q q-print %v v-print \n", "Hello World", "Hallo Welt", "Hola Mundo")
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "r.RequestURI is: %s\n", r.RequestURI)
+	fmt.Fprintf(w, "r.URL.Path is: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "r.URL is: %s\n", r.URL)
+	fmt.Fprintf(w, "r.URL.User is: %s\n", r.URL.User)
+	fmt.Fprintf(w, "r.URL.Host is: %s\n", r.URL.Host)
+	fmt.Fprintf(w, "r.URL.Fragment is: %s\n", r.URL.Fragment)
+}
+
+func main() {
+
+	http.HandleFunc("/v1/payments/", payments)
+
+	log.Fatalln(http.ListenAndServe(address, nil))
+
+}
